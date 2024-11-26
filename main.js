102eb34220c3d6d5254671486d01119eef82e689
Esraa Saeed <div.esraa.saeed@gmail.com>
2024-11-26 09:56:17 +0200

    trying a project

---
+++ main.js
@@ -0,0 +1,10 @@
+// function toggleMenu() {
+//         const navLinks = document.querySelector('.nav-links');
+//         navLinks.classList.toggle('active');
+//       }
+
+      function toggleMenu() {
+        const navbarNav = document.getElementById('navbarNav');
+        navbarNav.classList.toggle('show');
+      }
+      
